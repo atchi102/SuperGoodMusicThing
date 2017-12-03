f8788cce20eb6178087fca90ba40ee2c8c297bce
atchi102 <atchi102@mail.chapman.edu>
2017-12-02 18:16:26 -0800

    Added Auto incrementing to ids

--- tables.sql
+++ tables.sql
@@ -4,29 +4,35 @@ USE AtchisonColeFinal;
 
 CREATE TABLE Artists
 (
-	id INT PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
   name CHAR(50) NOT NULL UNIQUE,
 	label CHAR(20),
 	hometown CHAR(15),
 	genre CHAR(10)
 );
 
+ALTER TABLE Artists AUTO_INCREMENT=1000000;
+
 CREATE TABLE Genres
 (
-  id INT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
 	genre CHAR(15) UNIQUE
 );
 
+ALTER TABLE Genres AUTO_INCREMENT=2000000;
+
 CREATE TABLE Labels
 (
-  id INT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
 	name CHAR(20) UNIQUE,
 	location CHAR(25)
 );
 
+ALTER TABLE Labels AUTO_INCREMENT=3000000;
+
 CREATE TABLE Albums
 (
-	id INT PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	name CHAR(50) UNIQUE,
 	year INT,
 month INT,
@@ -44,6 +50,8 @@ FOREIGN KEY (artist) REFERENCES Artists(name)
 ON DELETE CASCADE
 ON UPDATE CASCADE
 );
+
+ALTER TABLE Albums AUTO_INCREMENT=4000000;
 
 
 CREATE TABLE Songs
@@ -65,10 +73,14 @@ CREATE TABLE Songs
 	ON UPDATE CASCADE
 );
 
+ALTER TABLE Songs AUTO_INCREMENT=5000000;
+
 CREATE TABLE Users(
-  id INT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   userName CHAR(20) UNIQUE
 );
+
+ALTER TABLE Users AUTO_INCREMENT=6000000;
 
 CREATE TABLE UserLikes
 (
@@ -77,7 +89,7 @@ CREATE TABLE UserLikes
 	FOREIGN KEY (userID) REFERENCES Users(id)
 	ON DELETE CASCADE
 	ON UPDATE CASCADE
-)
+);
 
 -- likeID will be the id of whatever the user liked (artist, album, song, playlist, user etc.)
 
@@ -107,12 +119,14 @@ ON DELETE SET NULL
 	FOREIGN KEY (labelID) REFERENCES Labels(id)
 	ON DELETE SET NULL
 	ON UPDATE CASCADE
-)
+);
 
 CREATE TABLE Recommendations
 (
-	id INT,
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	similarToID INT
-)
+);
+
+ALTER TABLE Recommendations AUTO_INCREMENT=7000000;
 
 -- Note:  The recommendation is a general id so that it can refer to an artist, album, or song and, if usability permits, a playlist
