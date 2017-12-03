f9a23f3c7557fc670b058138b178e05d288d580b
atchi102 <atchi102@mail.chapman.edu>
2017-12-03 15:36:57 -0800

    Updated automatic insert triggers

--- triggers.sql
+++ triggers.sql
@@ -11,17 +11,17 @@ END;//
 CREATE TRIGGER AlbumInsertTrigger BEFORE INSERT ON Songs
 FOR EACH ROW
 BEGIN
-IF(NOT NEW.album = NULL) THEN
+IF(NEW.album LIKE '%') THEN
 IF (NEW.album NOT IN (SELECT name FROM Albums)) THEN
 INSERT INTO Albums (name,artist) VALUES(NEW.album,NEW.artist);
 END IF;
 END IF;
 END;//
 
-CREATE TRIGGER GenreInsertTriggerArtists BEFORE INSERT ON Artists
+CREATE TRIGGER GenreInsertTriggerArtists BEFORE INSERT ON Songs
 FOR EACH ROW
 BEGIN
-IF(NOT NEW.genre = NULL) THEN
+IF(NEW.genre LIKE '%') THEN
 IF (NEW.genre NOT IN (SELECT genre FROM Genres)) THEN
 INSERT INTO Genres (genre) VALUES(NEW.genre);
 END IF;
@@ -35,5 +35,3 @@ IF(NEW.name IN (SELECT name FROM Songs WHERE name = NEW.name AND artist = NEW.ar
 DELETE FROM Songs WHERE name = NEW.name AND artist = NEW.artist;
 END IF;
 END;//
-
-INSERT INTO Songs (name,artist,album) VALUES ("Every Single Thing", "HOMESHAKE","Fresh Air")//
