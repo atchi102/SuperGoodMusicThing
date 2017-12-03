76215910ec886a1ad336d3c6fa43a5ff045bde64
atchi102 <atchi102@mail.chapman.edu>
2017-12-02 20:03:45 -0800

    Added dynamic addition of albums

--- triggers.sql
+++ triggers.sql
@@ -8,6 +8,14 @@ INSERT INTO Artists (name) VALUES(NEW.artist);
 END IF;
 END;//
 
+CREATE TRIGGER AlbumInsertTrigger BEFORE INSERT ON Songs
+FOR EACH ROW
+BEGIN
+IF (NEW.album NOT IN (SELECT name FROM Albums)) THEN
+INSERT INTO Albums (name) VALUES(NEW.album);
+END IF;
+END;//
+
 CREATE TRIGGER SongDuplicate BEFORE INSERT ON Songs
 FOR EACH ROW
 BEGIN
