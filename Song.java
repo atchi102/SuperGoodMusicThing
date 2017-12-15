48c46c1ebf5d54204dbb2dbb8823388b9a9a7ea3
atchi102 <atchi102@mail.chapman.edu>
2017-12-14 22:41:54 -0800

    Song class created

--- Song.java
+++ Song.java
@@ -12,9 +12,41 @@ public class Song
 
   public Song(int id, String name, String artist, String album, String genre, String songKey)
   {
-    
+    this.id = id;
+    this.name = name;
+    this.artist = artist;
+    this.album = album;
+    this.genre = genre;
+    this.songKey = genre;
   }
 
+  public getID()
+  {
+    return id;
+  }
 
+  public getName()
+  {
+    return name;
+  }
 
+  public getArtist()
+  {
+    return artist;
+  }
+
+  public getAlbum()
+  {
+    return album;
+  }
+
+  public getGenre()
+  {
+    return genre;
+  }
+
+  public getSongkey()
+  {
+    return songKey;
+  }
 }
